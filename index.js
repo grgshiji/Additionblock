7995803ac25d58987a87e870370aca36f4f955af
grgshiji <shiji@neoito.com>
2023-02-09 11:24:39 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const Additionblock = async (req, res) => {
+
+  // health check
+  if (req.params["health"] === "health") {
+    res.write(JSON.stringify({success: true, msg: "Health check success"}))
+    res.end()
+  }
+
+  // Add your code here
+  res.write(JSON.stringify({success: true, msg: `Hello Additionblock`}))
+  res.end()
+  
+}
+
+export default Additionblock
